574c7d9166758514fc11e644d5119a4e8345a2da
Mohamed Moumni <mmoumni@student.1337.ma>
2023-01-06 20:23:29 +0100

    adding adminer service in bonus

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# sed -i 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
 sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /tmp/init_db.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init_db.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';" >> /tmp/init_db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';" >> /tmp/init_db.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/init_db.sql
+
+mysql -u root < /tmp/init_db.sql
 
 exec "$@"
