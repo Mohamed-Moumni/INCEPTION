b8d6dd015aeb6cb15c62aceb0899027bab78cf01
Mohamed Moumni <mmoumni@student.1337.ma>
2023-01-10 11:19:30 +0100

    setup ftp successfully

--- srcs/requirements/bonus/FTP/tools/script.sh
+++ srcs/requirements/bonus/FTP/tools/script.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-useradd -m $FTP_USER
-
+# start vsftpd service
+service vsftpd start
+# create ftp user
+adduser --gecos "" $FTP_USER 
+# set password for ftp user
 echo "$FTP_USER:$FTP_PASSWORD_USER" | chpasswd
+# Create the ftp folder
+mkdir -p /home/$FTP_USER/ftp
+# set its ownership
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
+# Remove write permissions
+chmod a-w /home/$FTP_USER/ftp 
 
-mkdir -p /home/$FTP_USER/ftp # create ftp user
+# disables anonymous login to the FTP server
+echo "anonymous_enable=NO" >> /etc/vsftpd.conf;
+# enables write permissions for the FTP users.
+echo "write_enable=YES" >> /etc/vsftpd.conf;
+# 
+echo "chroot_local_user=YES" >> /etc/vsftpd.conf;
 
-chown nobody:nogroup /home/$FTP_USER/ftp # set its ownership
-
-chmod a-w /home/$FTP_USER/ftp # Remove write permissions
-
-mkdir -p /home/$FTP_USER/ftp/files # create the directory for file uploads
-
-chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files # ownership to the user
-
-# configuration modification
-
-sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
-
-sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/g' /etc/vsftpd.conf
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf;
 
 echo "user_sub_token=$FTP_USER" >> /etc/vsftpd.conf
+
+echo "local_enable=YES" >> /etc/vsftpd.conf
 
 echo "local_root=/home/$FTP_USER/ftp" >> /etc/vsftpd.conf
 
@@ -32,8 +36,10 @@ echo "userlist_enable=YES" >> /etc/vsftpd.conf
 
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 
+echo "userlist_deny=NO" >> /etc/vsftpd.conf
+
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
-mkdir -p /var/run/vsftpd/empty
+service vsftpd stop
 
 exec "$@"
